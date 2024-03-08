609b482cc1f6fdaa8a80f9142d7ef2043aa458f4
Fracco87 <fr.ussia@libero.it>
2024-03-08 15:32:42 +0100

    aggiunta

--- D5.js
+++ D5.js
@@ -97,6 +97,11 @@ const justTrims = []
 const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ]
+let i = 0;
+while (numericArray[i] !== 100 && i < numericArray.length) {
+  i++
+  console.log(numericArray[i])
+}
 
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
